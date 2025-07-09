03407d26a7ed750892c23c6f820866d053d3879b
24ci0224 <teru.ryu.abc@gmail.com>
2025-07-09 19:22:19 +0900

    no message

--- Butaball/Assets/Teru/script/Player.cs
+++ Butaball/Assets/Teru/script/Player.cs
@@ -1,13 +1,15 @@
 ﻿using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float accelaration;
     [SerializeField] PlayerInput action;
     [SerializeField] float rotateSpeed;
     [SerializeField] float mChragePow;
-    [SerializeField] float speed;  // 移動速度
+    [SerializeField] float mSpeed;// 移動速度
     float h,v;
     Rigidbody rb;
     InputAction move;
@@ -16,11 +18,15 @@ public class Player : MonoBehaviour
     float cTime;
     float chargePow;
     Vector3 moveDir;
+    Vector3 front;
+    float speed;
+    public LayerMask collisionMask;
     enum State
     {
         Idle,
         Move,
         Charge,
+        Inertia,
         Bound,
         Die,
     }
@@ -37,24 +43,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var inputAxis=move.ReadValue<Vector2>();
-        h = inputAxis.x;
-        v = inputAxis.y;   // W,S（-1〜1）
-
-        //カメラの正面を取得
-        Vector3 camForward = new Vector3(1, 0, 1);//Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-        //カメラの右側を取得
-        Vector3 camRight = new Vector3(1, 0, 1);//Vector3.Scale(Camera.main.transform.right, new Vector3(1, 0, 1)).normalized;
-        //移動方向を格納
-        moveDir = camForward * v + camRight * h;
-        moveDir.Normalize();
-        if (moveDir != Vector3.zero)
-        {
-            //進行方向に体を回転
-            rb.AddTorque(moveDir*rotateSpeed);
-        }
+        Angle();
         Think();
         Move();
+        Debug.Log(state);
     }
     void Think()
     {
@@ -66,10 +58,13 @@ public class Player : MonoBehaviour
                 break;
             case State.Move:
                 if (charge.IsPressed()) { state = State.Charge; }
-                if(move.ReadValue<Vector2>()==new Vector2(0,0)) { state=State.Bound; }
+                if(move.ReadValue<Vector2>()==new Vector2(0,0)) { state = State.Inertia; }
                 break;
             case State.Charge:
                 if (!charge.IsPressed()) { state = State.Bound; }
+                break;
+                case State.Inertia:
+                if (speed == 0) {state=State.Idle; }
                 break;
             case State.Bound:
                 if (bTime >= 1f) { state = State.Idle; bTime = 0; }
@@ -83,9 +78,7 @@ public class Player : MonoBehaviour
         switch (state)
         {
             case State.Move:
-                Vector3 vec = move.ReadValue<Vector2>();
-                Vector3 dir = new Vector3(vec.x,0,vec.y);
-                rb.AddForce( dir *  speed);
+                OnMove();
                 break;
             case State.Charge:
                 cTime += Time.deltaTime;
@@ -99,11 +92,68 @@ public class Player : MonoBehaviour
                     rb.AddForce(moveDir * mChragePow); 
                 }
                 break;
+             case State.Inertia:
+                float drag = 0.01f;
+                speed *= (1 - drag);
+                if(speed < 0.05) { speed = 0; }
+                rb.linearVelocity += moveDir  * Time.deltaTime;
+                break;
             case State.Bound:
                 bTime += Time.deltaTime;
                 break;
             case State.Die:
                 break;
         }
+    }
+    public void OnMove()
+    {
+        speed += accelaration*Time.deltaTime;
+        if (speed >= mSpeed) { speed= mSpeed; }
+        rb.linearVelocity += speed * moveDir * Time.deltaTime;
+    }
+    void Bound()
+    {
+        /*SphereCollider sphereCollider = GetComponent<SphereCollider>();
+        float radius = sphereCollider.radius * transform.localScale.x; // スケール込み
+        Vector3 rayOrigin = transform.position + moveDir.normalized * radius;*/
+        //Debug.DrawRay(transform.position, moveDir.normalized * , Color.red);
+        if (Physics.SphereCast(transform.position, 0.5f,transform.position, out RaycastHit hit, 0.1f, collisionMask))
+        {
+            moveDir = Vector3.Reflect(moveDir.normalized, hit.normal).normalized;
+            //state = State.Inertia;
+            Debug.Log("oppai");
+            transform.position = hit.point - moveDir.normalized * 0.05f;
+        }
+    }
+    void Angle()
+    {
+        var inputAxis = action.actions["Move"].ReadValue<Vector2>();
+        h = inputAxis.x;
+        v = inputAxis.y;   // W,S（-1〜1）
+        //カメラの正面を取得
+        Vector3 camForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        //カメラの右側を取得
+        Vector3 camRight = Vector3.Scale(Camera.main.transform.right, new Vector3(1, 0, 1)).normalized;
+        //移動方向を格納
+        moveDir = camForward * v + camRight * h;
+        moveDir.Normalize();
+
+        if (moveDir != Vector3.zero)
+        {
+            //進行方向に体を回転
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 bound = Vector3.Reflect(moveDir.normalized, collision.contacts[0].normal).normalized;
+        rb.AddForce(bound * speed*20);
+        moveDir = bound;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
     }
 }
