753d40b85750a3f687a129ffc68219d0a23f4f98
NokoAsh <takenoko702takeru@gmail.com>
2025-07-09 16:42:47 +0900

    背景の動きの追加 シーン遷移

---
+++ Butaball/Assets/Nokocha/Script/TitleCon.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleCon : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EndGame();
+    }
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene("StageSelect");
+    }
+    private void EndGame()
+    {
+        //Escが押された時
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+    Application.Quit();//ゲームプレイ終了
+#endif
+        }
+
+    }
+}
