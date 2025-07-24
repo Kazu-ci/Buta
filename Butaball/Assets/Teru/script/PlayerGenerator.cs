7307bc9763ce7b606792943ee080be96c3b51a26
Tel1urium <teru.abc.abc@gmail.com>
2025-07-24 12:27:33 +0900

    コントローラーでのエントリーできた

--- Butaball/Assets/Teru/script/PlayerGenerator.cs
+++ Butaball/Assets/Teru/script/PlayerGenerator.cs
@@ -1,60 +1,38 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
 using UnityEngine.InputSystem.Users;
 
 public class PlayerGenerator : MonoBehaviour
 {
     public GameObject[] playerPrefab;
-    private int cnt=0;
-    private void Awake()
-    {
-        /*if (Gamepad.all.Count < 2)
-        {
-            Debug.LogError("コントローラー2つが接続されていません。");
-            return;
-        }
-        gameObjects = new GameObject[Gamepad.all.Count];
-        int posX = -3;
-        // プレイヤー生成
-        for (int i = 0; i < Gamepad.all.Count; i++)
-        {
-            gameObjects[i] = PlayerInput.Instantiate(
-            playerPrefab[i],
-            playerIndex: i,
-            controlScheme: "Gamepad",
-            pairWithDevice: Gamepad.all[i]
-        ).gameObject;
-            gameObjects[i].GetComponent<Transform>().transform.position = new Vector3(posX, 0.5f, 0);
-            Debug.Log(gameObjects[i].transform.position);
-            posX = 3;
-        }*/
-    }
+    private int playerIndex = 0;
 
-    private void Update()
-    {
-        PlayerJoin();
-    }
-    public void PlayerJoin()
+    void Update()
     {
         foreach (var gamepad in Gamepad.all)
         {
-            // まだユーザーにペアリングされていないデバイスに対して
-            if (InputUser.FindUserPairedToDevice(gamepad) == null)
+            // すでにどこかのプレイヤーに使われていればスキップ
+            bool alreadyUsed = PlayerInput.all.Any(p => p.user.pairedDevices.Contains(gamepad));
+            if (alreadyUsed) continue;
+
+            // Aボタンが押されたら参加
+            if (gamepad.buttonSouth.wasPressedThisFrame)
             {
-                if (gamepad.buttonSouth.wasPressedThisFrame) //Aボタン
-                {
-                    if (cnt < playerPrefab.Length)
-                    {
-                        // プレイヤーを生成してペアリング
-                        var player = PlayerInput.Instantiate(playerPrefab[cnt],
-                        playerIndex: cnt,
-                        controlScheme: "Gamepad",
-                        pairWithDevice:gamepad);
-                        Debug.Log("Player joined with device: " + gamepad.deviceId);
-                        cnt++;
-                    }  
-                }
+                JoinPlayer(gamepad);
             }
         }
+    }
+
+    void JoinPlayer(Gamepad gamepad)
+    {
+        var playerInput = PlayerInput.Instantiate(
+        playerPrefab[playerIndex],
+        controlScheme: "Gamepad",  // Control Scheme 名はInputActionAssetに応じて
+        pairWithDevice: gamepad,
+        splitScreenIndex: playerIndex++
+    );
+        Debug.Log($"Player {playerIndex} joined with {gamepad.displayName}");
     }
 }
