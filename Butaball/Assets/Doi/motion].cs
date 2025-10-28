01768ebc10476960815acc0c5af01c4d290a4fd9
Tel1urium <teru.abc.abc@gmail.com>
2025-10-29 00:13:59 +0900

    最終調整

--- Butaball/Assets/Doi/motion].cs
+++ Butaball/Assets/Doi/motion].cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class motion] : MonoBehaviour
+public class motion : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
